a8937f847981cb08c8c91b20cd8656f18c81ab2a
Michael VanOverbeek <theplexgate@gmail.com>
2020-04-02 22:09:57 -0400

    initial check-in

---
+++ ShiftOS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShiftOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
